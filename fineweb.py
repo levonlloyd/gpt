f80f08e29f699ce27d26ff3f8e6f4b24f63e992b
Levon Lloyd <levon.lloyd@gmail.com>
2025-05-13 15:55:37 -0400

    fineweb update

--- fineweb.py
+++ fineweb.py
@@ -14,16 +14,6 @@ import tiktoken
 from datasets import load_dataset
 from tqdm import tqdm
 
-local_dir = "edu_fineweb10B"
-remote_name = "sample-10BT"
-shard_size = int(1e8)
-
-DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
-os.makedirs(DATA_CACHE_DIR, exist_ok=True)
-
-fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train")
-
-breakpoint()
 enc = tiktoken.get_encoding("gpt2")
 eot = enc._special_tokens["<|endoftext|>"]
 
@@ -43,39 +33,54 @@ def write_datafile(filename, tokens_np):
     np.save(filename, tokens_np)
 
 
-nprocs = max(1, os.cpu_count() // 2)
-nprocs = 1
-with mp.Pool(nprocs) as pool:
-    shard_index = 0
-    all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
-    token_count = 0
-    progress_bar = None
-    for tokens in pool.imap(tokenize, fw, chunksize=16):
-        if token_count + len(tokens) < shard_size:
-            all_tokens_np[token_count : token_count + len(tokens)] = tokens
-            token_count += len(tokens)
-            if progress_bar is None:
-                progress_bar = tqdm(
-                    total=shard_size, unit="tokens", desc=f"shard {shard_index}"
-                )
-            progress_bar.update(len(tokens))
-        else:
-            split = "val" if shard_index == 0 else "train"
-            filename = os.path.join(
-                DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}"
-            )
-            remainder = shard_size - token_count
-            progress_bar.update(remainder)
-            all_tokens_np[token_count : token_count + remainder] = tokens[:remainder]
-            write_datafile(filename, all_tokens_np)
-            shard_index += 1
-            progress_bar = None
-            all_tokens_np[0 : len(tokens) - remainder] = tokens[remainder:]
-            token_count = len(tokens) - remainder
+def main():
+    local_dir = "edu_fineweb10B"
+    remote_name = "sample-10BT"
+    shard_size = int(1e8)
 
-        if token_count != 0:
-            split = "val" if shard_index == 0 else "train"
-            filename = os.path.join(
-                DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}"
-            )
-            write_datafile(filename, all_tokens_np[:token_count])
+    DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
+    os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+
+    fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train")
+    nprocs = max(1, os.cpu_count() - 1)
+
+    with mp.Pool(nprocs) as pool:
+        shard_index = 0
+        all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
+        token_count = 0
+        progress_bar = None
+        for tokens in pool.imap(tokenize, fw, chunksize=128):
+            if token_count + len(tokens) < shard_size:
+                all_tokens_np[token_count : token_count + len(tokens)] = tokens
+                token_count += len(tokens)
+                if progress_bar is None:
+                    progress_bar = tqdm(
+                        total=shard_size, unit="tokens", desc=f"shard {shard_index}"
+                    )
+                progress_bar.update(len(tokens))
+            else:
+                split = "val" if shard_index == 0 else "train"
+                filename = os.path.join(
+                    DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}"
+                )
+                remainder = shard_size - token_count
+                progress_bar.update(remainder)
+                all_tokens_np[token_count : token_count + remainder] = tokens[
+                    :remainder
+                ]
+                write_datafile(filename, all_tokens_np)
+                shard_index += 1
+                progress_bar = None
+                all_tokens_np[0 : len(tokens) - remainder] = tokens[remainder:]
+                token_count = len(tokens) - remainder
+
+            if token_count != 0:
+                split = "val" if shard_index == 0 else "train"
+                filename = os.path.join(
+                    DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}"
+                )
+                write_datafile(filename, all_tokens_np[:token_count])
+
+
+if __name__ == "__main__":
+    main()
